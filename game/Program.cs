f2f5092fed19b9372e5ac8ceca5418a0ad6b91ee
Ignacio Cajiao <000930213@mohawkcollege.ca>
2024-03-31 17:11:08 -0400

    Add Ball Movement

--- game/Program.cs
+++ game/Program.cs
@@ -1,65 +1,82 @@
 ﻿using Raylib_cs;
 using System.Numerics;
-
-namespace game;
-
-internal class BallGame
+namespace game
 {
-    static string title = "Game Title";
-    static Vector2 position;
-    static Vector2 velocity;
-    static Vector2 gravity;
-    static Color color;
-    static int radius;
-
-    static void Main(string[] args)
+    internal class BallGame
     {
-        Raylib.InitWindow(800, 600, title);
- 
-        Raylib.SetTargetFPS(60);
+        static string title = "Ball Game";
+        static Vector2 position;
+        static Vector2 velocity;
+        static Vector2 gravity;
+        static Color color;
+        static int radius;
 
-        Setup();
-
-        while (!Raylib.WindowShouldClose())
+        static void Main(string[] args)
         {
+            Raylib.InitWindow(800, 600, title);
 
-            Raylib.BeginDrawing();
- 
-            Raylib.ClearBackground(Color.RayWhite);
+            Raylib.SetTargetFPS(60);
 
+            Setup();
 
-            Update();
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
 
- 
-            Raylib.EndDrawing();
+                Raylib.ClearBackground(Color.RayWhite);
+
+                Update();
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
         }
 
-        Raylib.CloseWindow();
-    }
-
-    static void Setup()
-    {
-        position = new Vector2(Raylib.GetScreenWidth(), Raylib.GetScreenHeight()) / 2;
-        color = Color.Blue;
-        radius = 25;
-
-        gravity = new Vector2(0, +10);
-    }
-
-    static void Update()
-    {
-        velocity += gravity * Raylib.GetFrameTime();
-        position += velocity;
-
-        bool isBelowScreen = position.Y + radius > +Raylib.GetScreenHeight();
-        bool isTravellingDown = position.Y > 0;
-
-        if (isBelowScreen && isTravellingDown)
+        static void Setup()
         {
-            position.Y = Raylib.GetScreenHeight() - radius;
-            velocity.Y = -velocity.Y * 0.8f;
+            position = new Vector2(Raylib.GetScreenWidth(), Raylib.GetScreenHeight()) / 2;
+            color = Color.Blue;
+            radius = 25;
+
+            gravity = new Vector2(0, +800);
         }
 
-        Raylib.DrawCircleV(position, radius, color);
+        static void Update()
+        {
+            
+            if (Raylib.IsKeyDown(KeyboardKey.Right))
+                velocity.X += 30;
+            if (Raylib.IsKeyDown(KeyboardKey.Left))
+                velocity.X -= 30;
+
+            velocity += gravity * Raylib.GetFrameTime();
+
+            position += velocity * Raylib.GetFrameTime();
+
+            bool isOnGround = position.Y + radius >= Raylib.GetScreenHeight();
+            if (isOnGround)
+            {
+                position.Y = Raylib.GetScreenHeight() - radius;
+                velocity.Y = 0;
+            }
+
+            if (Raylib.IsKeyPressed(KeyboardKey.Up) && isOnGround)
+                velocity.Y = -900;
+
+            if (position.X < 0 + radius)
+            {
+                position.X = 0 + radius;
+                velocity.X = 0;
+            }
+            else if (position.X > Raylib.GetScreenWidth() - radius)
+            {
+                position.X = Raylib.GetScreenWidth() - radius;
+                velocity.X = 0;
+            }
+
+            // Draw the ball
+            Raylib.DrawCircleV(position, radius, color);
+        }
     }
 }
