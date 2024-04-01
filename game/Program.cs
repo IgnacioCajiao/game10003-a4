893087a15ac7950a4d58d4567f04eb6ec0ce20a6
Ignacio Cajiao <000930213@mohawkcollege.ca>
2024-04-01 01:53:30 -0400

    Refactor Code

--- game/Program.cs
+++ game/Program.cs
@@ -6,34 +6,23 @@ namespace game
     internal class BallGame
     {
         static string title = "Ball Game";
-        static Vector2 position;
-        static Vector2 velocity;
-        static Vector2 gravity;
+        static Vector2 position, velocity, gravity;
         static Color color;
-        static int radius;
-        static int jumpCount = 5;
+        static int radius, jumpCount = 5, squareSize = 10, score = 0, highScore = 0;
         static bool isGameOver = false;
-        static Vector2[] squares = new Vector2[50]; 
-        static int squareSize = 10;
-        static int score = 0; 
-        static int highScore = 0; 
+        static Vector2[] squares = new Vector2[50];
 
         static void Main(string[] args)
         {
             Raylib.InitWindow(800, 600, title);
-
             Raylib.SetTargetFPS(60);
-
             Setup();
 
             while (!Raylib.WindowShouldClose())
             {
                 Raylib.BeginDrawing();
-
                 Raylib.ClearBackground(Color.RayWhite);
-
                 Update();
-
                 Raylib.EndDrawing();
             }
 
@@ -45,7 +34,6 @@ namespace game
             position = new Vector2(Raylib.GetScreenWidth(), Raylib.GetScreenHeight()) / 2;
             color = Color.Blue;
             radius = 25;
-
             gravity = new Vector2(0, +800);
 
             for (int i = 0; i < squares.Length; i++)
@@ -72,28 +60,47 @@ namespace game
         {
             if (isGameOver)
             {
-                Raylib.DrawText("Game Over", 300, 200, 45, Color.Black);
-                Raylib.DrawText($"Final Score: {score}", 300, 250, 30, Color.Black); 
-                Raylib.DrawText($"High Score: {highScore}", 300, 300, 30, Color.Black); 
-                Raylib.DrawText("Press Enter to Restart", 300, 350, 20, Color.Black); 
-
-                if (Raylib.IsKeyPressed(KeyboardKey.Enter))
-                {
-                    ResetGame();
-                }
-
+                DisplayGameOver();
+                if (Raylib.IsKeyPressed(KeyboardKey.Enter)) ResetGame();
                 return;
             }
 
-            if (Raylib.IsKeyDown(KeyboardKey.Right))
-                velocity.X += 30;
-            if (Raylib.IsKeyDown(KeyboardKey.Left))
-                velocity.X -= 30;
+            UpdateVelocity();
+            UpdatePosition();
+            CheckCollisions();
 
+            Raylib.DrawText($"Jumps left: {jumpCount}", 10, 10, 20, Color.Black);
+            Raylib.DrawText($"Score: {score}", 10, 30, 20, Color.Black); 
+
+            foreach (Vector2 square in squares)
+            {
+                if (square.X >= 0 && square.Y >= 0) 
+                {
+                    Raylib.DrawRectangleV(square, new Vector2(squareSize, squareSize), Color.Blue);
+                }
+            }
+
+            Raylib.DrawCircleV(position, radius, color);
+        }
+
+        static void DisplayGameOver()
+        {
+            Raylib.DrawText("Game Over", 300, 200, 45, Color.Black);
+            Raylib.DrawText($"Final Score: {score}", 300, 250, 30, Color.Black); 
+            Raylib.DrawText($"High Score: {highScore}", 300, 300, 30, Color.Black); 
+            Raylib.DrawText("Press Enter to Restart", 300, 350, 20, Color.Black); 
+        }
+
+        static void UpdateVelocity()
+        {
+            if (Raylib.IsKeyDown(KeyboardKey.Right)) velocity.X += 30;
+            if (Raylib.IsKeyDown(KeyboardKey.Left)) velocity.X -= 30;
             velocity += gravity * Raylib.GetFrameTime();
+        }
 
+        static void UpdatePosition()
+        {
             position += velocity * Raylib.GetFrameTime();
-
             bool isOnGround = position.Y + radius >= Raylib.GetScreenHeight();
 
             if (isOnGround)
@@ -104,11 +111,7 @@ namespace game
                 if (jumpCount <= 0)
                 {
                     isGameOver = true;
-            
-                    if (score > highScore)
-                    {
-                        highScore = score;
-                    }
+                    if (score > highScore) highScore = score;
                 }
             }
 
@@ -128,7 +131,10 @@ namespace game
                 position.X = Raylib.GetScreenWidth() - radius;
                 velocity.X = 0;
             }
+        }
 
+        static void CheckCollisions()
+        {
             for (int i = 0; i < squares.Length; i++)
             {
                 if (Raylib.CheckCollisionCircles(position, radius, squares[i] + new Vector2(squareSize / 2, squareSize / 2), squareSize / 2))
@@ -137,19 +143,6 @@ namespace game
                     score++;
                 }
             }
-
-            Raylib.DrawText($"Jumps left: {jumpCount}", 10, 10, 20, Color.Black);
-            Raylib.DrawText($"Score: {score}", 10, 30, 20, Color.Black); 
-
-            foreach (Vector2 square in squares)
-            {
-                if (square.X >= 0 && square.Y >= 0) 
-                {
-                    Raylib.DrawRectangleV(square, new Vector2(squareSize, squareSize), Color.Blue);
-                }
-            }
-
-            Raylib.DrawCircleV(position, radius, color);
         }
     }
 }
